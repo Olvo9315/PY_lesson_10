f8dd19e984fcc8fd0711b19c27173067c0ad96ac
olvo <alexvolenko9315@gmail.com>
2023-11-17 13:31:01 +0100

    one hot script

---
+++ one_hot.py
@@ -0,0 +1,20 @@
+# Задача 44: В ячейке ниже представлен код генерирующий DataFrame, которая состоит всего из 1 столбца. 
+# Ваша задача перевести его в one hot вид. Сможете ли вы это сделать без get_dummies?
+import pandas as pd
+import random
+lst = ['robot'] * 10
+lst += ['human'] * 10
+random.shuffle(lst)
+data = pd.DataFrame({'whoAmI':lst})
+# print(data.head())
+
+# Преобразование в one-hot c get_dummies:
+one_hot_encoded = pd.get_dummies(data['whoAmI'])
+result = pd.concat([data, one_hot_encoded], axis=1)
+
+# Преобразование в one-hot без get_dummies:
+mapp = {'robot': 0, 'human': 1}
+data['human'] = data['whoAmI'].map(mapp)
+data['robot'] = 1 - data['human']
+
+print(result.head(), '\n', data.head())
